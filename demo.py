759abd25489cd511a0d190afc75a93cc48a585ce
gongxi_cn_dl <56497007+gongxi-cn-ln-dl@users.noreply.github.com>
2020-08-14 14:18:05 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -32,3 +32,23 @@ cid = cid_dict['ConversationName']#Please replace 'ConversationName' with Conver
 print(dt.getMessage(cid))
 # return:
 # [['messageData', timestamp, openID, 'nick'],['messageData', timestamp, openID, 'nick'],['messageData', timestamp, openID, 'nick']]
+
+
+# 过滤聊天信息
+import DingTalk
+dt = DingTalk.Work()
+dt.Login()
+cid_dict = dt.getConversation()
+cid = cid_dict['ConversationName']#Please replace 'ConversationName' with ConversationName
+print(dt.getMessage(cid,mode='re',rule=['（(.*?)）','再见']))
+# return:
+# [['messageData', timestamp, openID, 'nick'],['messageData', timestamp, openID, 'nick'],['messageData', timestamp, openID, 'nick']]
+
+
+# 根据openid反查用户
+import DingTalk
+dt = DingTalk.Work()
+dt.Login()
+dt.getUserInfo(openid)
+# return:
+# 啊我不想打了我只能告诉你name在['body']['cardUserModel']['name']
